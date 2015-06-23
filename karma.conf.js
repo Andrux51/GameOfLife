08a598167bb308a002f0ec401ddfb41e9603e8bb
Andy Martin <andymartin51@gmail.com>
2015-06-23 13:39:17 -0600

    add code coverage reporting

--- karma.conf.js
+++ karma.conf.js
@@ -31,13 +31,22 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      '*.js': ['coverage']
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
+
+    coverageReporter: {
+      dir: '.',
+      reporters: [
+        { type: 'html', subdir: 'report-html' },
+	{ type: 'lcovonly', subdir: '.', file: 'report-lcovonly.txt' }
+      ]
+    },
 
 
     // web server port
